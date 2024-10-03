c7d2c408e85ca66f2d2adff42340bbc073ca6a52
Konstantin <90984821+KotSka666@users.noreply.github.com>
2024-10-03 07:47:15 +0300

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,5 @@
+number_of_homework_assignments_completed = 12
+number_of_hours_spent = 1.5
+course_title = 'Python'
+time_for_one_task = number_of_hours_spent / number_of_homework_assignments_completed
+print (f"Курс: {course_title}\nВсего задач:{number_of_homework_assignments_completed}\nЗатрачено часов:{number_of_hours_spent}\nСреднее время выполнения:{time_for_one_task}")
